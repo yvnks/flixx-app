7999823ad482437e8520a983c4df740ee7e82a69
Jeff Yankson <jjyankson19@gmail.com>
2026-05-11 10:57:36 +0000

    Fetch data from the backend and generate HTML with Javascript

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     case "/":
     case "./index.html":
       console.log("Home");
+      renderPopularMovies();
       break;
     case "/shows.html":
       console.log("Shows");
@@ -22,8 +23,62 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function hightlightActiveLink() {
   const links = document.querySelectorAll(".nav-link").forEach((link) => {
-    if (link.getAttribute('href') === global.location) {
+    if (link.getAttribute("href") === global.location) {
       link.classList.add("active");
     }
   });
+}
+
+/**
+ * @this function renders
+ * the popular movies.
+ */
+
+async function renderPopularMovies() {
+  let html = "";
+  const { results } = await fetchDataFromAPI("movie/popular");
+  console.log(results)
+
+  results.forEach((movie) => {
+    html += `
+    <div class="card">
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img src="https://tmdb.org/t/p/w500${movie.poster_path}" 
+                    class="card-img-top" alt="${movie.title}"
+                 />`
+                : `<img src="images/no-image.jpg" 
+                    class="card-img-top" alt="Movie Title"
+                  />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+    </div>
+    `;
+  });
+  document.querySelector("#popular-movies").innerHTML = html;
+  console.log(html)
+}
+
+/**
+ * @this function fetches data
+ * from the API.
+ */
+async function fetchDataFromAPI(endpoint) {
+  const API_URL = "https://api.themoviedb.org/3/";
+  const API_KEY = "bc2f421c810659588237b20b4fce4f00";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`,
+  );
+
+  const data = response.json();
+
+  return data;
 }
